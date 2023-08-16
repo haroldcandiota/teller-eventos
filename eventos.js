91ec184f3f4f376665133914ab0827f82995299e
haroldcandiota <141259644+haroldcandiota@users.noreply.github.com>
2023-08-16 17:52:39 -0300

    trabajo completo

---
+++ eventos.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let miDiv = document.getElementById("miDiv");
+
+    miDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div.");
+    });
+});
+
+
+
+
+
